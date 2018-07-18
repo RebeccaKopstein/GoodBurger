1b459ba61b29c1fd611eb4ebdd3c14bef99887fa
Becca K <rkopstein@gmail.com>
2018-07-18 09:51:50 -0400

    Adding Jaws DB

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: "root",
-    database: 'goodburgers_db',
+    host: 'g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'djz0sbzyru7avmsq',
+    password: "nwl8xzf3z5jouui5",
+    database: 'evf3tgaindz0vzm4',
 });
 
 connection.connect(function (err) {
